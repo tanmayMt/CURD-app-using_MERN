c661ae2253a801f8fc7152e580aef229577c4888
web-collab <tanmoy587@gmail.com>
2025-04-03 13:29:17 +0530

    mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+//connnect mongodb
+connectDb();
+
 app.get("/", (req, res) => {
     res.status(200).send("<h1>Welcome To the Server of CRUD APP using MERN </h1>");
 });
-
 // Handle all undefined routes
 app.use((req, res) => {
     res.status(404).send("<h1>Route Not Found</h1>");
 });
 
+
+
 app.listen(PORT, () => {
-    console.log(`🚀 Server is Running at http://localhost:${PORT}`);
+    console.log(`🚀 Server is Running at http://localhost:${PORT}`);//mongodb+srv://tanmoy:123@cluster0.6epn77t.mongodb.net/
 });
