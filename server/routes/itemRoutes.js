1e4d235325b778be9c01a6d2f2cd24b161923a01
web-collab <tanmoy587@gmail.com>
2025-04-11 20:56:19 +0530

    jwt.verify  Middleware for JWT Authentication

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -4,6 +4,10 @@ import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router() ;
 
+router.get('/protected', authMiddleware, (req, res) => {
+  res.status(202).send(`User ID: ${req.userId} & Email ID: ${req.email}`);
+});
+
 router.post("/",async(req,res)=>{
     const {name, description, price} = req.body;
     const newItem = new Item({name, description, price});
@@ -16,8 +20,6 @@ router.get("/", authMiddleware, async(req,res)=>{
     })
 })
 
-router.get('/protected', authMiddleware, (req, res) => {
-  res.status(202).send(`User ID: ${req.userId} & Email ID: ${req.email}`);
-});
+
 
 export default router;
