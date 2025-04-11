257e66e953fbf82201ff61acd8d588ac5af7c479
web-collab <tanmoy587@gmail.com>
2025-04-11 21:49:30 +0530

    Add New Item (Authenticated User

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -8,9 +8,27 @@ router.get('/protected', authMiddleware, (req, res) => {
   res.status(202).send(`User ID: ${req.userId} & Email ID: ${req.email}`);
 });
 
-router.post("/",async(req,res)=>{
-    const {name, description, price} = req.body;
-    const newItem = new Item({name, description, price});
+router.post("/",authMiddleware,async(req,res)=>{
+    try {
+        const {name, description, price} = req.body;
+        // Create a new item associated with the authenticated user's ID
+        const newItem = new Item({name, description, price, userId:req.userId});
+        await newItem.save();//Save item to mongodb
+        // Send back the created item with 201 (Created) status
+        res.status(201).send({
+            success:true,
+            data: newItem,
+            message:"Item is added Successfully"
+        });
+    }
+    catch (error)
+    {
+        res.status(404).send({
+            success:false,
+            message: "Server Error in Adding Item"
+        })
+    }
+    
 })
 
 router.get("/", authMiddleware, async(req,res)=>{
